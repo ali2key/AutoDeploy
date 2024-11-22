0d5ceb8c169035b90211df15ab91278296fe4843
ali2key <64281880+ali2key@users.noreply.github.com>
2024-11-22 20:57:19 +0100

    Update oobe.ps1

--- oobe.ps1
+++ oobe.ps1
@@ -33,7 +33,7 @@ if ($IsOOBEComplete) {
 # # # # # # # # # # # # # # # # # # # # ################ 
 # Select URI based on user input
 Write-Host "Select an answer file to use:" -ForegroundColor Cyan
-Write-Host "1. Regular Employees (https://example1.com)"
+Write-Host "1. Set Hostname (https://raw.githubusercontent.com/ali2key/AutoDeploy/refs/heads/main/autounattend-win11pro-setHostname.xml)"
 Write-Host "2. For Sale (https://shorturl.at/QNklR)"
 Write-Host "3. Contractors (https://example3.com)"
 
@@ -41,7 +41,7 @@ $uri = ""
 do {
     $choice = Read-Host "Enter the number of your choice (1, 2, or 3)"
     switch ($choice) {
-        "1" { $uri = "https://example1.com"; break }
+        "1" { $uri = "https://raw.githubusercontent.com/ali2key/AutoDeploy/refs/heads/main/autounattend-win11pro-setHostname.xml"; break }
         "2" { $uri = "https://shorturl.at/QNklR"; break }
         "3" { $uri = "https://example3.com"; break }
         default { Write-Host "Invalid selection. Please choose 1, 2, or 3." -ForegroundColor Red }
